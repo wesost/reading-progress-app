11f6b01980bb5bc433e8551bca122826bc15adf4
Wes Ostlund <wesost11@gmail.com>
2025-10-02 09:20:31 -0700

    added delete user endpoint, updated delete review endpoint so admin can delete any review

--- src/main/java/com/example/grclone/services/ReviewService.java
+++ src/main/java/com/example/grclone/services/ReviewService.java
@@ -1,7 +1,6 @@
 package com.example.grclone.services;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
